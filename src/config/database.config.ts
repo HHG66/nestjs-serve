c0c108f66978e58238a1cb370537bec6dba44483
han <2421578700@qq.com>
2025-01-02 09:44:45 +0800

    1、数据库配置

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: HHG
  * @Date: 2023-12-15 09:19:06
- * @LastEditTime: 2024-12-21 11:40:06
+ * @LastEditTime: 2025-01-02 09:43:45
  * @LastEditors: 韩宏广
- * @FilePath: /financial-serve/src/config/database.config.ts
+ * @FilePath: \financial-serve\src\config\database.config.ts
  * @文件说明:
  */
 
@@ -17,7 +17,7 @@ export default () =>{
     }
   : {
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/website-test',
-      MONGODB_URI: 'mongodb://127.0.0.1:27017/financial-dev',
+      MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
       serverSelectionTimeoutMS: 5000,  // 设置超时时间
     };
     return config
