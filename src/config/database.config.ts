932718c88f8aeeeb21facb6c0e6a49728ba77fa3
han <2421578700@qq.com>
2025-01-02 18:28:34 +0800

    超时配置

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: HHG
  * @Date: 2023-12-15 09:19:06
- * @LastEditTime: 2025-01-02 16:20:23
+ * @LastEditTime: 2025-01-02 17:33:58
  * @LastEditors: 韩宏广
  * @FilePath: \financial-serve\src\config\database.config.ts
  * @文件说明:
@@ -19,7 +19,7 @@ export default () =>{
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/website-test',
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
       MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
-      serverSelectionTimeoutMS: 50000,  // 设置超时时间
+      serverSelectionTimeoutMS: 5000,  // 设置超时时间
     };
     return config
 }
