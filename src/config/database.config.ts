00f6f1e3f7ae346cb81f5d2d5c6446605ff36632
han <2421578700@qq.com>
2025-01-03 17:20:53 +0800

    修改数据库连接配置

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: HHG
  * @Date: 2023-12-15 09:19:06
- * @LastEditTime: 2025-01-02 23:41:05
+ * @LastEditTime: 2025-01-03 17:11:20
  * @LastEditors: 韩宏广
- * @FilePath: /financial-serve/src/config/database.config.ts
+ * @FilePath: \financial-serve\src\config\database.config.ts
  * @文件说明:
  */
 
@@ -20,8 +20,8 @@ export default () =>{
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/website-test',
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
-      // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
-      MONGODB_URI: 'mongodb://han:han1314.@1.25.137.190:27018/financial',
+      MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
+      // MONGODB_URI: 'mongodb://han:han1314.@1.25.137.190:27018/financial',
       
       serverSelectionTimeoutMS: 5000,  // 设置超时时间
     };
