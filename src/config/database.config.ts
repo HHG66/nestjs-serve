5115dc1f5389c6dd5f0b303b8ed446eb79ed713f
韩宏广 <2421578700@qq.com>
2025-02-23 22:11:33 +0800

    公网ip数据库连接

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: HHG
  * @Date: 2023-12-15 09:19:06
- * @LastEditTime: 2025-02-23 21:50:44
+ * @LastEditTime: 2025-02-23 22:10:35
  * @LastEditors: 韩宏广
  * @FilePath: /financial-serve/src/config/database.config.ts
  * @文件说明:
@@ -20,9 +20,9 @@ export default () =>{
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/website-test',
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
       // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
-      MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
+      // MONGODB_URI: 'mongodb://han:han1314.@192.168.0.101:27017/financial',
       // MONGODB_URI: 'mongodb://han:han1314.@127.0.0.1:27017/financial',
-      // MONGODB_URI: 'mongodb://han:han1314.@1.25.137.190:27018/financial',
+      MONGODB_URI: 'mongodb://han:han1314.@1.25.137.190:27018/financial',
       // MONGODB_URI: 'mongodb://192.168.0.101:27017/financial',
 
       
